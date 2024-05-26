3086d1d5e659ab7aceb4575acaec4c459da6c5b6
Estuardo07 <xavihernand7@hotmail.com>
2024-05-25 23:42:17 -0600

    Home page successfully implemented

--- src/components/Inicio/Botones/Botones.jsx
+++ src/components/Inicio/Botones/Botones.jsx
@@ -3,8 +3,7 @@ import styles from '../Botones/botones.module.css';
 const Botones = () => {
     return(
       <div className={styles.Botones}>
-       <button><a className={styles.a} target='_blank' href="https://mail.google.com/mail/u/0/?pli=1#inbox?compose=CllgCJTNHSCmLGXmjRjlthDCCdvbBSmkQjcKJvmPrTdvGPvPLLBFVWrVqnfjrSLNvfPWGXSvMZL">GMAIL</a></button>
-       <button><a className={styles.a} target='_blank'  href="https://drive.google.com/file/d/1fsLDnhIrlZqY1i3r2WeER9PSeKB9sj5X/view?usp=sharing">CV</a></button>
+       <button><a className={styles.a} target='_blank'  href="https://drive.google.com/file/d/1j0kDwc4385dax_ZU9pwbAnjEgI1UpGPF/view?usp=drive_link">CV</a></button>
       </div> 
     )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,12 +14,12 @@ const Nav = () => {
     return(
         <header className={Styles.header}> 
             
-            <span className={Styles.logo}>Javier Hernández</span> 
+            <span className={Styles.logo}>Javi</span> 
 
           <nav className={Styles.nav}>
-            <Link to='/portafolio/'><span>H</span>ome</Link>
-            <Link to='/portafolio/proyectos'><span>P</span>rojects</Link>
-            <Link to='/portafolio/sobremi'><span>A</span>bout<span>-</span>Me</Link>
+            <Link to='/portafolio/'><span>I</span>nicio</Link>
+            <Link to='/portafolio/proyectos'><span>P</span>royectos</Link>
+            <Link to='/portafolio/sobremi'><span>S</span>obre <span>M</span>i</Link>
           </nav>
 
           <nav className={Styles.nav_mobile}>
