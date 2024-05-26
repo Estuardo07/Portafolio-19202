d2d80ecb7ead43943e12cb8f3fdb23f7ef111286
Estuardo07 <xavihernand7@hotmail.com>
2024-05-26 00:23:36 -0600

    Projects page successfully implemented

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -2,8 +2,15 @@ import styles from "../Proyectos/Proyectos.module.css";
 import img1 from "../../assets/html.png";
 import img2 from "../../assets/css.png";
 import img3 from "../../assets/js.png";
-
-
+import img4 from "../../assets/node.png";
+import img5 from "../../assets/react.png";
+import img6 from "../../assets/mysql.png";
+import img7 from "../../assets/code.png";
+import img8 from "../../assets/web.png";
+import img9 from "../../assets/design.png";
+import github from "../../assets/github.svg";
+import page from "../../assets/url.svg";
+import youtube from "../../assets/youtube.svg";
 
 const Proyectos = () => {
   const tarjetas = [
@@ -11,82 +18,84 @@ const Proyectos = () => {
       image: [img1],
       name: "Lab 3: HTML Only",
       descrip: "Proyecto hecho solo con html.",
+      tecno: [img1],
       url: "https://arpanetos.lol",
-      urlGithub: "https://github.com",
+      urlGithub: "https://github.com/Estuardo07/Lab3-19202.git",
     },
     
     {
       image: [img2],
       name: "Lab 4: CSS Only",
       descrip: "Proyecto hecho solo con css.",
+      tecno: [img1, img2],
       url: "https://arpanetos.lol",
-      urlGithub: "https://github.com",
+      urlGithub: "https://github.com/Estuardo07/Lab4-19202.git",
     },
 
     {
       image: [img3],
       name: "Lab 5: JavaScript Only",
       descrip: "Proyecto hecho solo con javascript.",
+      tecno: [img1, img2, img3],
       url: "https://arpanetos.lol",
-      urlGithub: "https://github.com",
+      urlGithub: "https://github.com/Estuardo07/Lab5-19202.git",
     },
   ];
 
   return (
     <section className={styles.Proyectos}>
       <div className={styles.servicios}>
-        <h2>Technologies</h2>
+        <h2>Tecnologías</h2>
         <div className={styles.habi}>
           <div>
-            <img src={logo2} alt="logo html" />
+            <img src={img1} alt="logo html" />
             <h5>HTML</h5>
           </div>
 
           <div>
-            <img src={logo5} alt="logo css" />
+            <img src={img2} alt="logo css" />
             <h5>CSS</h5>
           </div>
 
           <div>
-            <img src={logo4} alt="logo js" />
-            <h5>JAVASCRIPT</h5>
+            <img src={img3} alt="logo js" />
+            <h5>JavaScript</h5>
           </div>
 
           <div>
-            <img src={logo7} alt="logo node" />
-            <h5>NODE</h5>
+            <img src={img4} alt="logo node" />
+            <h5>Node JS</h5>
           </div>
 
           <div>
-            <img src={logo1} alt="logo react" />
-            <h5>REACT</h5>
+            <img src={img5} alt="logo react" />
+            <h5>React JS</h5>
           </div>
 
           <div>
-            <img src={logo8} alt="logo" />
-            <h5>PostgreSQL</h5>
+            <img src={img6} alt="logo" />
+            <h5>MySQL</h5>
           </div>
         </div>
 
         <div>
-          <h2 className={styles.h2S}>Services</h2>
+          <h2 className={styles.h2S}>Servicios</h2>
           <div className={styles.metodo}>
             <div>
-              <img src={code} alt="imagen code" />
+              <img src={img7} alt="imagen code" />
               <p>
-                Development of specific solutions for your modern web
-                applications.
+                Desarrollo de soluciones específicas para tus aplicaciones web.
               </p>
             </div>
 
             <div>
-              <img src={web} alt="imagen web" />
-              <p>Technological projects adapted to your needs.</p>
+              <img src={img8} alt="imagen web" />
+              <p>Proyectos tecnológicos adaptados a tus necesidades.</p>
             </div>
 
             <div>
-              <img src={desing} alt="imagen desing" />
-              <p>Responsive design for all devices</p>
+              <img src={img9} alt="imagen desing" />
+              <p>Diseño responsivo para todos los dispositivos.</p>
             </div>
           </div>
         </div>
